17ac89b2f3e4899834f96da211ff1fca713e5e13
Alessandro Boussiron <alessandro.boussiron@epitech.eu>
2026-03-18 00:20:57 +0100

    refactor: reorganize dynamic array implementation into utils directory

--- src/utils/get_error.c
+++ src/utils/get_error.c
@@ -8,66 +8,30 @@
 #include "main.h"
 #include <stdio.h>
 
-static int print_einp_error(const char *message, const char *detail)
-{
-    if (message) {
-        fprintf(stderr, "Input error : %s\n", message);
-    } else {
-        fprintf(stderr, "Input error\n");
-    }
-    if (detail)
-        fprintf(stderr, "Details : %s\n", detail);
-    return EINP;
-}
-
-static int print_elex_error(const char *message, const char *detail)
-{
-    if (message) {
-        fprintf(stderr, "Lexer error : %s\n", message);
-    } else {
-        fprintf(stderr, "Lexer error\n");
-    }
-    if (detail)
-        fprintf(stderr, "Details : %s\n", detail);
-    return ELEX;
-}
-
-static int print_epar_error(const char *message, const char *detail)
-{
-    if (message) {
-        fprintf(stderr, "Parser error : %s\n", message);
-    } else {
-        fprintf(stderr, "Parser error\n");
-    }
-    if (detail)
-        fprintf(stderr, "Details : %s\n", detail);
-    return EPAR;
-}
-
-static int print_egen_error(const char *message, const char *detail)
-{
-    if (message) {
-        fprintf(stderr, "Code generation error : %s\n", message);
-    } else {
-        fprintf(stderr, "Code generation error\n");
-    }
-    if (detail)
-        fprintf(stderr, "Details : %s\n", detail);
-    return EGEN;
-}
-
 int get_error(int code, const char *message, const char *detail)
 {
     switch (code) {
         case EINP:
-            return print_EINP_error(message, detail);
+            fprintf(stderr, "Implementation error");
+            break;
         case ELEX:
-            return print_ELEX_error(message, detail);
+            fprintf(stderr, "Lexer error");
+            break;
         case EPAR:
-            return print_EPAR_error(message, detail);
+            fprintf(stderr, "Parser error");
+            break;
         case EGEN:
-            return print_EGEN_error(message, detail);
+            fprintf(stderr, "Generation error");
+            break;
         default:
             return code;
     }
+    if (message) {
+        fprintf(stderr, ": %s\n", message);
+    } else {
+        fprintf(stderr, "\n");
+    }
+    if (detail)
+        fprintf(stderr, "Details : %s\n", detail);
+    return code;
 }
